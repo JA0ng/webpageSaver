b6d5441a473742e3fa4a23d088da1e387f4b8acd
John Ong <haddock-pay0d@icloud.com>
2023-12-11 20:57:44 -0800

    Bug fix added filename sanitization and included -r tag for read command
    curl was having issues saving some websites due to special characters in the title tag, introduced filename sanitization to fix issue
    
    added -r tag to read command (https://github.com/koalaman/shellcheck/wiki/SC2162)

--- webpageSaver.zsh
+++ webpageSaver.zsh
@@ -3,37 +3,37 @@
 # Config File
 CONFIG_FILE="config.txt"
 
-while true; do
-  # Check if the config file exists
-  if [ -f "$CONFIG_FILE" ]; then
-    # Load config from file
-    source "$CONFIG_FILE"
-  else
-    # Prompt the user for SAVE_DIRECTORY if the config file doesn't exist
-    echo -n "What directory would you like to save your files to? "
-    read SAVE_DIRECTORY < /dev/tty
+# Check if the config file exists
+if [ -f "$CONFIG_FILE" ]; then
+  # Load config from file
+  source "$CONFIG_FILE"
+else
+  # Prompt the user for SAVE_DIRECTORY if the config file doesn't exist
+  echo -n "What directory would you like to save your files to? "
+  read -r SAVE_DIRECTORY < /dev/tty
 
-    # Check if the directory exists
-    if [ ! -d "$SAVE_DIRECTORY" ]; then
-      echo -n "Directory does not exist. Would you like to create it (y/n): "
-      read create_dir < /dev/tty
-      if [ "$create_dir" = "y" ]; then
-        mkdir -p "$SAVE_DIRECTORY"
-        echo "Directory created: $SAVE_DIRECTORY"
-      else
-        echo "Exiting. Directory not created."
-        exit 1
-      fi
+  # Check if the directory exists
+  if [ ! -d "$SAVE_DIRECTORY" ]; then
+    echo -n "Directory does not exist. Would you like to create it (y/n): "
+    read -r create_dir < /dev/tty
+    if [ "$create_dir" = "y" ]; then
+      mkdir -p "$SAVE_DIRECTORY"
+      echo "Directory created: $SAVE_DIRECTORY"
+    else
+      echo "Exiting. Directory not created."
+      exit 1
     fi
-
-    # Create the config file with specified SAVE_DIRECTORY
-    echo "SAVE_DIRECTORY=\"$SAVE_DIRECTORY\"" > "$CONFIG_FILE"
-    echo "Config file created: $CONFIG_FILE"
   fi
 
+  # Create the config file with specified SAVE_DIRECTORY
+  echo "SAVE_DIRECTORY=\"$SAVE_DIRECTORY\"" > "$CONFIG_FILE"
+  echo "Config file created: $CONFIG_FILE"
+fi
+
+while true; do
   # Prompt user for a URL
   echo -n "Enter the URL of the webpage you want to save: "
-  read url < /dev/tty
+  read -r url < /dev/tty
 
   # Check if URL is provided
   if [ -z "$url" ]; then
@@ -44,17 +44,20 @@ while true; do
   # Extract title from the webpage
   title=$(curl -s "$url" | grep -o '<title>[^<]*' | sed -E 's/<title>([^<]*)<\/title>/\1/' | sed -E 's/^[[:space:]]+//;s/[[:space:]]+$//;s/<[^>]+>//g')
 
+  # Replace spaces and special characters in the title with underscores
+  sanitized_title="${title//[^a-zA-Z0-9]/}"
+
   # Create timestamp for the filename
   timestamp=$(date +"%Y%m%d%H%M%S")
 
   # Use curl to download the webpage
-  curl -o "${SAVE_DIRECTORY}/${title}_${timestamp}.html" "$url"
+  curl -o "${SAVE_DIRECTORY}/${sanitized_title}_${timestamp}.html" "$url"
 
-  echo "Webpage saved as ${SAVE_DIRECTORY}/${title}_${timestamp}.html"
+  echo "Webpage saved as ${SAVE_DIRECTORY}/${sanitized_title}_${timestamp}.html"
 
   # Ask the user if they want to download another website
   echo -n "Do you want to download another website? (y/n): "
-  read download_another < /dev/tty
+  read -r download_another < /dev/tty
 
   if [ "$download_another" != "y" ]; then
     echo "Exiting."
