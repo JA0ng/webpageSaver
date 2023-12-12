59c1b903823eb2ff793450b394cc334cf1f0c6e1
John Ong <haddock-pay0d@icloud.com>
2023-12-11 16:46:53 -0800

    Add looping to script to allow user to download multiple webstites
    Update README

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This script is a simple webpage downloader that uses `curl` to download webpages. It prompts you for a URL and a directory to save the downloaded webpage as an HTML file.
 
+I created this script to help my workflow when keeping track of job's I've applied to. 
+In the past I've had recruiters/hiring managers reach out to me about a role I applied to only to find that the original job posting to be taken down leaving me in the dark about the role itself.
+To combat this I began saving the job postings as webarchives when applying.
+Getting the pathname for the saved archive was cumbersome so I created this script to streamline the process.
+
 ## Prerequisites
 
 - [Zsh](https://www.zsh.org/) shell
@@ -19,6 +24,9 @@ This script is a simple webpage downloader that uses `curl` to download webpages
 4. View the Result:
    - The webpage will be downloaded and saved as an HTML file in the specified directory.
    - The script will display a message indicating where the webpage has been saved.
+   - The script will ask if you would like to download another website
+     - `y` repeat from step 3
+     - `n' script exits
 
 ## Configuration
 - The script uses a configuration file (config.txt) to store the default save directory. If the file doesn't exist, the script will prompt you to enter the directory.
--- webpageSaver.zsh
+++ webpageSaver.zsh
@@ -3,47 +3,58 @@
 # Config File
 CONFIG_FILE="config.txt"
 
-# Check if the config file exists
-if [ -f "$CONFIG_FILE" ]; then
-  # Load config from file
-  source "$CONFIG_FILE"
-else
-  # Prompt the user for SAVE_DIRECTORY if the config file doesn't exist
-  echo -n "What directory would you like to save your files to? "
-  read SAVE_DIRECTORY < /dev/tty
+while true; do
+  # Check if the config file exists
+  if [ -f "$CONFIG_FILE" ]; then
+    # Load config from file
+    source "$CONFIG_FILE"
+  else
+    # Prompt the user for SAVE_DIRECTORY if the config file doesn't exist
+    echo -n "What directory would you like to save your files to? "
+    read SAVE_DIRECTORY < /dev/tty
 
-  # Check if the directory exists
-  if [ ! -d "$SAVE_DIRECTORY" ]; then
-    echo -n "Directory does not exist. Would you like to create it (y/n): "
-    read create_dir < /dev/tty
-    if [ "$create_dir" = "y" ]; then
-      mkdir -p "$SAVE_DIRECTORY"
-      echo "Directory created: $SAVE_DIRECTORY"
-    else
-      echo "Exiting. Directory not created."
-      exit 1
+    # Check if the directory exists
+    if [ ! -d "$SAVE_DIRECTORY" ]; then
+      echo -n "Directory does not exist. Would you like to create it (y/n): "
+      read create_dir < /dev/tty
+      if [ "$create_dir" = "y" ]; then
+        mkdir -p "$SAVE_DIRECTORY"
+        echo "Directory created: $SAVE_DIRECTORY"
+      else
+        echo "Exiting. Directory not created."
+        exit 1
+      fi
     fi
+
+    # Create the config file with specified SAVE_DIRECTORY
+    echo "SAVE_DIRECTORY=\"$SAVE_DIRECTORY\"" > "$CONFIG_FILE"
+    echo "Config file created: $CONFIG_FILE"
   fi
 
-  # Create the config file with specified SAVE_DIRECTORY
-  echo "SAVE_DIRECTORY=\"$SAVE_DIRECTORY\"" > "$CONFIG_FILE"
-  echo "Config file created: $CONFIG_FILE"
-fi
+  # Prompt user for a URL
+  echo -n "Enter the URL of the webpage you want to save: "
+  read url < /dev/tty
 
-# Prompt user for a URL
-echo -n "Enter the URL of the webpage you want to save: "
-read url < /dev/tty
+  # Check if URL is provided
+  if [ -z "$url" ]; then
+    echo "URL is required. Exiting."
+    exit 1
+  fi
 
-# Check if URL is provided
-if [ -z "$url" ]; then
-  echo "URL is required. Exiting."
-  exit 1
-fi
+  # Create timestamp for the filename
+  timestamp=$(date +"%Y%m%d%H%M%S")
 
-# Create timestamp for the filename
-timestamp=$(date +"%Y%m%d%H%M%S")
+  # Use curl to download the webpage
+  curl -o "${SAVE_DIRECTORY}/page_${timestamp}.html" "$url"
 
-# Use curl to download the webpage
-curl -o "${SAVE_DIRECTORY}/page_${timestamp}.html" "$url"
+  echo "Webpage saved as ${SAVE_DIRECTORY}/page_${timestamp}.html"
 
-echo "Webpage saved as ${SAVE_DIRECTORY}/page_${timestamp}.html"
+  # Ask the user if they want to download another website
+  echo -n "Do you want to download another website? (y/n): "
+  read download_another < /dev/tty
+
+  if [ "$download_another" != "y" ]; then
+    echo "Exiting."
+    exit 0
+  fi
+done
